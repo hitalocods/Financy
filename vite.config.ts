6ca7ea4ac5d3d1574b96252a67bd5e27b1f1fbbf
Hitalo <hitalolacerdag@gmail.com>
2026-03-24 20:34:53 -0300

    Add Vercel configuration

--- vite.config.ts
+++ vite.config.ts
@@ -162,9 +162,10 @@ export default defineConfig({
   },
   envDir: path.resolve(import.meta.dirname),
   root: path.resolve(import.meta.dirname, "client"),
+  publicDir: path.resolve(import.meta.dirname, "client/public"),
   build: {
-    outDir: path.resolve(import.meta.dirname, "dist/public"),
-    emptyOutDir: true,
+    outDir: path.resolve(import.meta.dirname, "dist"),
+  emptyOutDir: true,
   },
   server: {
     port: 3000,
